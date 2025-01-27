d31536b8457f27aa93a317ae7d3b777d37d18668
Aksh-shipit <snehaagarwal1904@gmail.com>
2025-01-27 04:39:14 +0000

    stringsDone

---
+++ Example1/strings.js
@@ -0,0 +1,18 @@
+const name="hitesh"
+const repCount=50
+console.log(name+repCount+"value");
+console.log(`Hello my name is ${name} and my repoCount is ${repCount}`);
+const gameName=new String("hiteshhc")
+console.log(gameName[0]);
+console.log(gameName._proto_);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+const newString=gameName.substring(0,4);
+console.log(newString);
+const anotherString=gameName.slice(-7,4);
+console.log(anotherString);
+const newStringOne="   hitesh   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
