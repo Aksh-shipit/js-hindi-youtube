62d14e5661b0fc08063d92346ef026f69bb68548
Aksh-shipit <snehaagarwal1904@gmail.com>
2025-01-28 11:48:02 +0000

    changes_done

---
+++ Example1/array.js
@@ -0,0 +1,15 @@
+const myArr=[1,2,3,4,5]
+const myHeroes=["Shaktiman","nagraj"]
+const myArr2=new Array(1,2,3,4)
+console.log(myArr[0]);
+//array methods
+// myArr.push(6)
+// myArr.pop()
+//myArr.unshift(9);
+//myArr.shift()
+//myArr.shift()
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(9));
+const newArr=myArr.join();
+console.log(myArr);
+console.log(typeof newArr);
---
+++ Example1/dates_in.js
@@ -0,0 +1,11 @@
+let mydate=new Date();
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+
+
+//let myCreatedDate=new Date(2023,0,23,5,3)
+let myCreatedDate=new Date("2023-01-14")
+console.log(myCreatedDate);
+console.log(myCreatedDate.toLocaleString);
